4c974531cc66a2884583f5893a83f40bb370ff65
__stan__ <8445726+schanjr@users.noreply.github.com>
2022-12-23 17:47:56 +0800

    test

--- home.py
+++ home.py
@@ -1,14 +1,17 @@
+import os
+import sys
+current_path = os.getcwd()
+sys.path.append(os.path.join(current_path, "tools"))
+print(sys.path)
+
 import pandas as pd
 import pydeck as pdk
 import streamlit as st
 from tools import calculations as calc
 
-import os
-import sys
 
-current_path = os.getcwd()
-sys.path.append(os.path.join(current_path, "tools"))
-print(sys.path)
+
+
 
 st.set_page_config(layout="wide")
 
